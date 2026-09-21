7bd9870ae22c204e4b8dd9c88e69f387d500b335
ptw-star <phoebetangw@gmail.com>
2026-09-21 10:21:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ setup() {
     
     const githubConfig = ref(JSON.parse(localStorage.getItem('github_config')) || { owner: '', repo: '', token: '' });
     const exchangeRates = ref(JSON.parse(localStorage.getItem('exchange_rates')) || {
-        baMa: 0.05511, fei: 0.26, yee: 0.051558
+        baMa: 0.25, fei: 0.26, yee: 0.25
     });
 
     const scheduleData = ref({});
